fe99269607a87c922d38353423da494fac7df80b
alid49263 <alid49263@gmail.com>
2021-04-08 21:01:50 +0530

    updated mobile view

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 import {Snake_Speed as speed,update as updateSnake,draw as drawSnake,snakeHead, intersect} from "./snake.js"
 import {food as drawFood,update as updateFood,Score} from './food.js'
+import {mobileList} from './input.js'
 const gameboard=document.getElementById('gameboard');
 const score=document.getElementById('points')
+const highscore=document.getElementById('hs')
+const top=document.getElementById('top')
+const left=document.getElementById('left')
+const bottom=document.getElementById('bottom')
+const right=document.getElementById('right')
 let gameover=false
+mobileList(top,bottom,left,right)
 setInterval(()=>{
     update()
     draw()
@@ -12,16 +19,23 @@ function update(){
     updateFood()
     score.innerHTML=`${Score}`
     lose()
+    if(Score>localStorage.getItem("highscore")){
+        localStorage.setItem("highscore", Score);
+     }
     if(gameover){
+        
         window.location.reload()
         alert("Game Over, Press OK to Restart.")
     }
+    
 }
 function draw(){
     gameboard.innerHTML=''
     drawSnake(gameboard);
+    
     drawFood(gameboard);
 }
 function lose(){
     gameover=snakeHead()||intersect()
 }
+highscore.innerHTML=localStorage.getItem("highscore")
--- input.js
+++ input.js
@@ -1,5 +1,23 @@
 let inputDirection={x:0,y:0}
 let lastinput={x:0,y:0}
+export function mobileList(top,bottom,left,rigth){
+    top.addEventListener('click',()=>{
+        if(lastinput.y!==0)return
+        inputDirection={x:0,y:-1}
+    })
+    left.addEventListener('click',()=>{
+        if(lastinput.x!==0)return
+        inputDirection={x:-1,y:0}
+    })
+    bottom.addEventListener('click',()=>{
+        if(lastinput.y!==0)return
+        inputDirection={x:0,y:1}
+    })
+    right.addEventListener('click',()=>{
+        if(lastinput.x!==0)return
+        inputDirection={x:1,y:0}
+    })
+}
 window.addEventListener('keydown',e=>{
     switch(e.key){
         case 'ArrowUp':
